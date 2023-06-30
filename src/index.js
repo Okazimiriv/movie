2cb7a71aab0a835999fdb0377442017d965166dc
Oksana Komarnytska <okazimiriv@gmail.com>
2023-06-30 16:16:58 +0200

    2

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 import 'modern-normalize';
-// import './styles.css';
+import './styles.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/movie">
       <App />
     </BrowserRouter>
   </StrictMode>
